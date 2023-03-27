f7c9197024255087025c4b7a773cf2eabea2765b
Krylov_AY <moscow_city231@rambler.ru>
2023-03-27 17:01:34 +0300

    Исправлено

---
+++ Aircraft_controller/Dispetcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Airplane_exam
+{
+       class Dispatcher
+    {
+        public string name { get; set; }
+        public Dispatcher(string name)
+        {
+            this.name=name;
+        }
+        public event Flydelegate fly_event;
+        public event DelegPoints delegpoints;
+
+        public void Flight_correction(int speed, int height)
+        {
+            if (fly_event!=null)
+            {
+                fly_event(speed, height);
+            }
+        }
+
+        public void PointsEvent(int recomend_heidht, int height, int height_comparison)
+        {
+            if (delegpoints!=null)
+            {
+                delegpoints(recomend_heidht, height, height_comparison);
+            }
+        }
+
+        public void Exeptions_null(int speed_or_height)
+        {
+            try
+            {
+                if (speed_or_height <= 0)
+                    throw new Exception("Самолет разбился, скорость или высота не должны быть равными 0");
+            }
+            catch (Exception ex)
+            {
+                Clear();
+                WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+
+    }
+}
--- Aircraft_controller/Program.cs
+++ Aircraft_controller/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
 using System.IO;
 
@@ -11,149 +8,6 @@ namespace Airplane_exam
     public delegate void Flydelegate(int speed, int height);
     public delegate void DelegPoints(int recomend_heidht, int height, int height_comparison);
 
-    class Airplane
-    {
-        string fPath = "Black_Box.txt";
-        string str;
-        protected int speed;
-        public int Myspeed
-        {
-            get { return speed; }
-            set { speed = value; }
-        }
-        protected int height;
-
-        public int Myheight
-        {
-            get { return height; }
-            set { height = value; }
-        }
-        private int points;
-
-        public int Mypoints
-        {
-            get { return points; }
-            set { points = value; }
-        }
-
-        public void Flight_correction(int speed, int height)
-        {
-
-            SetCursorPosition(0, 0);
-            WriteLine("***********************************************************");
-            Write(str = $"скорость самолёта составляет {speed} км*ч высота {height} км\n");
-            WriteLine("***********************************************************");
-            using (FileStream fs = new FileStream(fPath, FileMode.Append, FileAccess.Write, FileShare.Write))
-            {
-                byte[] str_byte = Encoding.UTF8.GetBytes(str);
-                fs.Write(str_byte, 0, str_byte.Length);
-            }
-        }
-
-        public void Points_penal(int recomend_heidht, int height, int height_comparison)
-        {
-            SetCursorPosition(0, 5);
-            if (recomend_heidht != height && height_comparison > 300 && height_comparison <= 600)
-            {
-                Write(str = $"Штрафные баллы - {points += 25}\n");
-                WriteLine("***********************************************************");
-
-            }
-            else if (recomend_heidht != height && height_comparison > 600 && height_comparison < 1300)
-            {
-                Write(str = $"Штрафные баллы - {points += 50}\n");
-                WriteLine("***********************************************************");
-            }
-            try
-            {
-                if (points > 1000)
-                    throw new Exception(str = "Непригоден к полетам, вы набрали штафных баллов более 1000");
-            }
-            catch (Exception ex)
-            {
-                Clear();
-                WriteLine(ex.Message);
-                Environment.Exit(0);
-            }
-            using (FileStream fs = new FileStream(fPath, FileMode.Append, FileAccess.Write, FileShare.Write))
-            {
-                byte[] str_byte = Encoding.UTF8.GetBytes(str);
-                fs.Write(str_byte, 0, str_byte.Length);
-            }
-        }
-
-        public Airplane(int speed, int height, int points)
-        {
-            this.speed = speed;
-            this.height = height;
-            this.points = points;
-        }
-        public void Penalty_points_height(int height, int height_comparison)
-        {
-            try
-            {
-                if (height > 1300)
-                    throw new Exception(str = "Непригоден к полетам, вы набрали недопустимую высоту");
-                if (height_comparison > 1300)
-                    throw new Exception(str = "Самолет разбился, разница между рекомендуемой и текущей высотой более 1300 км");
-            }
-            catch (Exception ex)
-            {
-                Clear();
-                WriteLine(ex.Message);
-                Environment.Exit(0);
-            }
-            using (FileStream fs = new FileStream(fPath, FileMode.Append, FileAccess.Write, FileShare.Write))
-            {
-                byte[] str_byte = Encoding.UTF8.GetBytes(str);
-                fs.Write(str_byte, 0, str_byte.Length);
-            }
-        }
-
-    }
-
-    class Dispatcher
-    {
-        public string name { get; set; }
-        public Dispatcher(string name)
-        {
-            this.name=name;
-        }
-        public event Flydelegate fly_event;
-        public event DelegPoints delegpoints;
-
-        public void Flight_correction(int speed, int height)
-        {
-            if (fly_event!=null)
-            {
-                fly_event(speed, height);
-            }
-        }
-
-        public void PointsEvent(int recomend_heidht, int height, int height_comparison)
-        {
-            if (delegpoints!=null)
-            {
-                delegpoints(recomend_heidht, height, height_comparison);
-            }
-        }
-
-        public void Exeptions_null(int speed_or_height)
-        {
-            try
-            {
-                if (speed_or_height <= 0)
-                    throw new Exception("Самолет разбился, скорость или высота не должны быть равными 0");
-            }
-            catch (Exception ex)
-            {
-                Clear();
-                WriteLine(ex.Message);
-                Environment.Exit(0);
-            }
-        }
-
-    }
 
     internal class Program
     {
@@ -370,7 +224,6 @@ namespace Airplane_exam
                 distance += 50;
                 dispatcher2.delegpoints += air.Points_penal;
                 air.Points_penal(recomend_heidht, air.Myheight, height_comparison);
-                points = air.Mypoints;
                 ReadKey();
 
             } while (distance != 950);
